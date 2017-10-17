8843eee50a827d28dbc40146809470cb63be0c6d
Sean Clarke <xseanclarke@gmail.com>
2017-10-17 15:31:28 -0400

    Strings

--- types.py
+++ types.py
@@ -5,3 +5,21 @@ pi = 3.14159
 print(answer + pi) # = 45.141.159 // No need to worry about conversion
 print(int(pi)) # == 3
 print(float(answer)) # == 42.0
+
+# Strings
+
+'Hello World' == "Hello World" == """Hello World"""
+"hello".capitalize == "Hello"
+"hello".replace("e", "a") == "hallo"
+"hello".isalpha() == True
+"123".isdigit() == True # Useful when converting to digit
+"some,csv,values".split(",") == ["some","csv","values"]
+
+# String Format Function
+
+name = "PythonBo"
+machine = "HAL"
+
+"Nice to meeet you {0}. I am {1}".format(name, machine) # where 0 == value name & 1 == value of machine
+# Python 3.6 Supports String Interpolation
+f"Nice to meeet you {name}. I am {machine}"
