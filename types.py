2d34823c8c7abf6a350f742ead2b19bea3b2f4ce
Sean Clarke <xseanclarke@gmail.com>
2017-10-17 15:32:01 -0400

    Boolean and None

--- types.py
+++ types.py
@@ -23,3 +23,17 @@ machine = "HAL"
 "Nice to meeet you {0}. I am {1}".format(name, machine) # where 0 == value name & 1 == value of machine
 # Python 3.6 Supports String Interpolation
 f"Nice to meeet you {name}. I am {machine}"
+
+# Boolean and None
+
+# - Most often used in IF statements and flagging if something has / hasn't been done
+python_course = True
+java_course = False
+# Capital Letters are necessary in these declarations
+
+int(python_course) == 1
+int(java_course) == 0
+
+str(python_course) == "True"
+
+aliens_found = None # Useful as placeholder variable to later have value / equivalent to false
