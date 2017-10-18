7a267e8819c78b756caa85e4aa951d54256772db
Sean Clarke <xseanclarke@gmail.com>
2017-10-18 00:21:50 -0400

    function arguments

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ int("15") # == 15
 username = input("Enter the user's name: ")
 
     # Some examples of functions in python:
-
+students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
@@ -22,13 +22,31 @@ def print_students_titlecase():
     students_titlecase = get_students_titlecase()
     print(students_titlecase)
 
-def add_student(name):
+def add_student(name, student_id=332): # sets a default student_id if one is not provided / ideally would be unique and random
+    student = {"name": name, "student_id": student_id }
     students.append(name)
+
+def var_args(name, *args):
+    print(name)
+    print(args)
+
+def var_kw_args(name, **kwargs): # Allows you to target specific values
+    print(name)
+    print(kwargs["description"], kwargs["feedback"])
 
 student_list = get_students_titlecase()
 
-add_student("Mark")
+add_student(name="Mark", student_id=15)
+
+var_args("Mark", "Loves Python", None, "Hello", True)
+
+var_kw_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
 
 # Topic End
 print("–––––––––––––––––––––––––––")
 print("\n")
+
+
+
+
+# Function Arguments
