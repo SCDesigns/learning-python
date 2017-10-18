e6f65ad21e21c377f2d9c8a57df00e00ba806b2e
Sean Clarke <xseanclarke@gmail.com>
2017-10-18 00:32:47 -0400

    adding students to our app

--- functions.py
+++ functions.py
@@ -1,21 +1,14 @@
 # Our App - PyStudentManager
-# Function Arguments
-print("* Functions")
+# Adding Students to Our App
+print("* Adding Students to Our App")
 print("–––––––––––––––––––––––––––")
-
-    # Of course we've alreadt seen some of Python's built in functions
-
-print("Hello World")
-str(3) # == "3"
-int("15") # == 15
-username = input("Enter the user's name: ")
 
     # Some examples of functions in python:
 students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 def print_students_titlecase():
@@ -24,23 +17,15 @@ def print_students_titlecase():
 
 def add_student(name, student_id=332): # sets a default student_id if one is not provided / ideally would be unique and random
     student = {"name": name, "student_id": student_id }
-    students.append(name)
-
-def var_args(name, *args):
-    print(name)
-    print(args)
-
-def var_kw_args(name, **kwargs): # Allows you to target specific values
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
+    students.append(student)
 
 student_list = get_students_titlecase()
 
-add_student(name="Mark", student_id=15)
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
 
-var_args("Mark", "Loves Python", None, "Hello", True)
-
-var_kw_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
+add_student(student_name, student_id)
+print_students_titlecase()
 
 # Topic End
 print("–––––––––––––––––––––––––––")
